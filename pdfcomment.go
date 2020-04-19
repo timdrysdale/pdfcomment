79b6cec6e0ecbf0d9eb6ae0dcb975265e9f1354a
Tim Drysdale <timothy.d.drysdale@gmail.com>
2020-04-19 23:47:28 +0100

    getting data out of annotation

--- pdfcomment.go
+++ pdfcomment.go
@@ -9,6 +9,7 @@ package pdfcomment
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 
 	"github.com/timdrysdale/geo"
 	pdfcore "github.com/unidoc/unipdf/v3/core"
@@ -27,77 +28,44 @@ type Comments map[int][]Comment
 // this doesn't know about pages ...?!!
 func GetComments(reader *pdf.PdfReader) (Comments, error) {
 
-	comments := make(Comments)
+	comments := make(map[int][]Comment)
 
-	//numPages, err := reader.GetNumPages()
-	//if err != nil {
-	//	return comments, err
-	//}
-
-	for _, page := range reader.PageList {
+	for p, page := range reader.PageList {
 
 		if annotations, err := page.GetAnnotations(); err == nil {
 
 			for _, annot := range annotations {
 
 				if reflect.TypeOf(annot.GetContext()).String() == "*model.PdfAnnotationText" {
-					//					fmt.Println(annot.GetContext())
 
 					fmt.Println(annot.Contents)
 					fmt.Println(annot.Rect)
+
 					if rect, is := annot.Rect.(*pdfcore.PdfObjectArray); is {
 
 						fmt.Printf("%v %v %v %v\n", rect.Get(0), rect.Get(1), rect.Get(2), rect.Get(3))
-					}
 
-					//
-					//newComment := &Comment{
-					//	Pos:    geo.Point{X: annot.Rect.Get(0), Y: annot.Rect.Get(1)},
-					//	Text:   annot.Contents.String(),
-					//	Author: dict.Get("T"),
-					//	Page:   p,
-					//}
+						x, err := strconv.ParseFloat(rect.Get(0).String(), 64)
+						if err != nil {
+							return comments, err
+						}
+						y, err := strconv.ParseFloat(rect.Get(1).String(), 64)
+						if err != nil {
+							return comments, err
+						}
+
+						newComment := Comment{
+							Pos:  geo.Point{X: x, Y: y},
+							Text: annot.Contents.String(),
+							Page: p,
+						}
+
+						comments[p] = append(comments[p], newComment)
+
+					}
 
 				}
 
-				//	fmt.Println(reflect.TypeOf(annot.GetContext()).String())
-				//
-				//	fmt.Printf("Parent: %v\n", annot.StructParent)
-				//	fmt.Printf("Context: %v\n", annot.GetContext())
-				//	//if at, is := annot.(*pdf.PdfAnnotationText); is {
-				//	//	fmt.Println(reflect.TypeOf(at).String())
-				//	//}
-				//	//var v *pdf.PdfAnnotationText
-				//	//fmt.Println(reflect.TypeOf(v).String())
-				//	fmt.Println(reflect.TypeOf(annot).String())
-				//
-				//	// see pdfcore/primitives
-				//	//annot.Contents is *core.PdfObjectString
-				//	//annot.Rect is *core.PdfObjectArray
-				//
-				//	fmt.Printf("%v\n", annot)
-				//
-				//	if rect, is := annot.Rect.(*pdfcore.PdfObjectArray); is {
-				//
-				//		fmt.Printf("%v %v %v %v\n", rect.Get(0), rect.Get(1), rect.Get(2), rect.Get(3))
-				//	}
-				//
-				//	fmt.Printf("%T\n\n", annot.Contents)
-
-				//fmt.Printf("%T\n", annot.Rect)
-				//if text, is := annot.(*pdf.PdfAnnotationText); is {
-				//	fmt.Printf("%v\n", annot)
-				//}
-
-				/*name := fmt.Sprintf("%s", annot.Get("Name"))
-				if strings.Compare(name, "Comment") == 0 {
-					newComment = &Comment{
-						Pos:    geo.Point{X: 0, Y: 0},
-						Text:   dict.Get("Contents"),
-						Author: dict.Get("T"),
-						Page:   p,
-					}
-				*/
 			}
 
 		}
